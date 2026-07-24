3d0bcb1d9ac79d4fece2a29d6249dced01dc2e04
강재홍 (Kang Jaehong) <j.kang@navercorp.com>
2026-07-24 13:29:47 +0900

    Release 1.3.15

---
+++ sample/6000.2.8f1-UnitySample/Assets/NGSDK/Plugins/iOS/NNGSDK.xcframework/ios-arm64/NNGSDK.framework/Headers/NNGSDK.h
@@ -0,0 +1,14 @@
+//
+//  NNGSDK.h
+//  NNGSDK
+//
+//  Copyright © 2026 NAVER Corp. All rights reserved.
+//  Unauthorized use, modification and redistribution of this software are strongly prohibited.
+//
+
+#ifndef NNGSDK_h
+#define NNGSDK_h
+
+#import <NNGSDK/NNGSDKManager.h>
+
+#endif /* NNGSDK_h */
